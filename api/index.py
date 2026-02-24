4afddb291a043410173dd748e769a34dd597b7cf
TotiManuel <manuel.mandaio@gmail.com>
2026-02-24 05:25:43 -0300

    Try Fix

--- api/index.py
+++ api/index.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-from models.__init__ import init_db
+from models import init_db
 
 app = Flask(
     __name__,
--- models/database.py
+++ models/database.py
@@ -3,7 +3,11 @@ from sqlalchemy.orm import declarative_base, sessionmaker
 
 DATABASE_URL = "sqlite:///database.db"
 
-engine = create_engine(DATABASE_URL, echo=True)
+engine = create_engine(
+    DATABASE_URL,
+    echo=True,
+    connect_args={"check_same_thread": False}
+)
 
 SessionLocal = sessionmaker(bind=engine)
 
