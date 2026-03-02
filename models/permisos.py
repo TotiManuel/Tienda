0f9198aafb12d46fb16af3562ba2aa622c53df97
TotiManuel <manuel.mandaio@gmail.com>
2026-03-02 04:43:38 -0300

    Fix Permisos

--- models/permisos.py
+++ models/permisos.py
@@ -1,4 +1,14 @@
 PERMISOS = {
+    "superadmin": [
+        "ver_dashboard",
+        "ver_inventario_admin",
+        "eliminar_producto_admin",
+        "crear_producto_admin",
+        "gestionar_usuarios",
+        "gestionar_empresas",
+        "ver_reportes",
+        "configuracion"
+    ],
     "admin": [
         "ver_dashboard",
         "gestionar_usuarios",
